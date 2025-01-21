0a269127278bf174732e47e8519a900f6b38595a
Parker <98468440+Maninaction231p@users.noreply.github.com>
2025-01-21 13:51:54 +0000

    hgf

--- pk.py
+++ pk.py
@@ -18,52 +18,58 @@ st.set_page_config(
 )
 
 
-st.title("Chat with ChaMage") 
+with st.expander('See Details',expanded=True):
+    st.title("Upload Image") 
+    st.markdown("To Unleash your creativity with Chamage, the innovative image-based chatbot. Share your favorite images, and Chamage will use them as inspiration to craft unique and imaginative responses. Explore new ideas, spark your creativity, and have fun conversing with Chamage!")
 
 
 with st.sidebar:
     uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
-    image = Image.open(uploaded_file)
-    st.image(image, width=275 ,use_container_width =False)
+    if uploaded_file:
+        image = Image.open(uploaded_file)
+        st.image(image, width=275 ,use_container_width =False)
+
+if uploaded_file:
+    st.title("Chat with ChaMage") 
 
 
-# Choose a Gemini model.
-model = genai.GenerativeModel(model_name="gemini-1.5-flash-latest")
+    # Choose a Gemini model.
+    model = genai.GenerativeModel(model_name="gemini-1.5-flash-latest")
 
-# Create a prompt.
-prompt = "Give context to the image."
-response = model.generate_content([image, prompt])
-print(response.text)
+    # Create a prompt.
+    prompt = "Give context to the image."
+    response = model.generate_content([image, prompt])
+    print(response.text)
 
-# Initialize chat history
-if "messages" not in st.session_state:
-    st.session_state.messages = [{"role": "assistant", "content": f"{response.text} 👇"}]
+    # Initialize chat history
+    if "messages" not in st.session_state:
+        st.session_state.messages = [{"role": "assistant", "content": f"{response.text} 👇"}]
 
 
-# Display chat messages from history on app rerun
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
-    print(st.session_state.messages)
+    # Display chat messages from history on app rerun
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+        print(st.session_state.messages)
 
-# Create a prompt.
-response = model.generate_content([image, prompt])
-print(response.text)
+    # Create a prompt.
+    response = model.generate_content([image, prompt])
+    print(response.text)
 
-prompt = st.chat_input("Type a message...", key="message_input")
-# Accept user input
-if prompt :
-    # Add user message to chat history
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    # Display user message in chat message container
-    with st.chat_message("user"):
-        st.markdown(prompt)
+    prompt = st.chat_input("Type a message...", key="message_input")
+    # Accept user input
+    if prompt :
+        # Add user message to chat history
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        # Display user message in chat message container
+        with st.chat_message("user"):
+            st.markdown(prompt)
 
-    # Display assistant response in chat message container
-    with st.chat_message("assistant"):
-        message_placeholder = st.empty()
-        # Simulate stream of response with milliseconds delay
-        message_placeholder.markdown(response.text)
-    # Add assistant response to chat history
-    st.session_state.messages.append({"role": "assistant", "content": response.text})
+        # Display assistant response in chat message container
+        with st.chat_message("assistant"):
+            message_placeholder = st.empty()
+            # Simulate stream of response with milliseconds delay
+            message_placeholder.markdown(response.text)
+        # Add assistant response to chat history
+        st.session_state.messages.append({"role": "assistant", "content": response.text})
 
